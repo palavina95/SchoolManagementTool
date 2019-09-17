9281bb8cfbf53d7f919db21c9042862dfee5ec3e
palavina95 <vivian.bridy@students.hevs.ch>
2019-09-17 13:01:33 +0300

    First commit

---
+++ src/main/java/com/example/schoolmanagementtool/SchoolmanagementtoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementtoolApplication.class, args);
+	}
+
+}
